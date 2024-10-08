42056d1fa61e829758fcd730fa8cacae03c34594
Nate Gibson <00nathan.gibson00@gmail.com>
2024-10-08 15:12:43 +1300

    Granting Abilities

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraGameplayAbility.cpp
@@ -0,0 +1,5 @@
+// Copyright CrunchyHuman
+
+
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+
--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -10,6 +10,16 @@ void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 
 }
 
+void UAuraAbilitySystemComponent::AddCharacterAbilities(const TArray<TSubclassOf<UGameplayAbility>>& StartupAbility)
+{
+	for (TSubclassOf<UGameplayAbility> AbilityClass : StartupAbility)
+	{
+		FGameplayAbilitySpec AbilitySpec = FGameplayAbilitySpec(AbilityClass, 1);
+		//GiveAbility(AbilitySpec);
+		GiveAbilityAndActivateOnce(AbilitySpec);
+	}
+}
+
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
 {
 	FGameplayTagContainer TagContainer;
