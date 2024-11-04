a4ca8c5db95afd524e903a4ba984f38fa7d28dc3
Nate Gibson <00nathan.gibson00@gmail.com>
2024-11-05 12:43:17 +1300

    Damage Meta Attribute

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -146,6 +146,14 @@ public:
 	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Mana);
 
 	/*
+	 * Meta Attributes
+	 */
+
+	UPROPERTY(BlueprintReadOnly, Category = "Meta Attributes")
+	FGameplayAttributeData IncomingDamage;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, IncomingDamage);
+
+	/*
 	 * Primary Attributes Functions
 	 */
 
